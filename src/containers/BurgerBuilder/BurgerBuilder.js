daf5531bc690107e27e96a234f21d59c1fa907b9
bishalpd <bishal@gmail.com>
2019-04-17 18:01:19 +0600

    modal added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,10 +6,10 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
-  salad:0.5,
-  cheese:0.4,
-  meat:1.3,
-  bacon:0.7
+    salad:0.5,
+    cheese:0.4,
+    meat:1.3,
+    bacon:0.7
 };
 
 class BurgerBuilder extends Component{
@@ -22,13 +22,14 @@ class BurgerBuilder extends Component{
             meat:0
         },
         totalPrice:4,
-        purchasable:false
+        purchasable:false,
+        purchasing:false
     };
 
     updatePurchaseState(ingredients){
         const sum = Object.keys(ingredients)
             .map(igKey => {
-               return ingredients[igKey]
+                return ingredients[igKey]
             })
             .reduce((sum,el)=>{
                 return sum + el;
@@ -68,30 +69,40 @@ class BurgerBuilder extends Component{
         this.updatePurchaseState(updatedIngredients);
     };
 
-render(){
-
-    const disabledInfo = {
-        ...this.state.ingredients
+    purchaseHandler =()=>{
+        this.setState({purchasing:true});
     };
-    for(let key in disabledInfo){
-        disabledInfo[key] = disabledInfo[key] <= 0
+
+    purchaseCancelHandler = () =>{
+        this.setState({purchasing:false})
     }
 
-    return(
-        <Aux>
-            <Modal>
-                <OrderSummary ingredients={this.state.ingredients}/>
-            </Modal>
-            <Burger ingredients={this.state.ingredients}/>
-            <BuildControls
-                ingredientAdded={this.addIngredientHandler}
-                ingredientRemoved={this.removeIngredientHandler}
-                disabled={disabledInfo}
-                purchasable={this.state.purchasable}
-                price={this.state.totalPrice}
-            />
-        </Aux>
-    )
-}
+    render(){
+
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for(let key in disabledInfo){
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
+        return(
+            <Aux>
+                <Modal
+                    modalClosed={this.purchaseCancelHandler}
+                    show={this.state.purchasing}>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
+                    price={this.state.totalPrice}
+                />
+            </Aux>
+        )
+    }
 }
 export default BurgerBuilder;
